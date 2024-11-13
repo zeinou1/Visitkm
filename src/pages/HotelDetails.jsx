65488212c4d6df334a393d1db3a18c7e9992fa9b
abdoulkader <zarikader@gmail.com>
2024-11-13 15:50:16 +0100

    add file style.css

--- src/pages/HotelDetails.jsx
+++ src/pages/HotelDetails.jsx
@@ -1,5 +1,5 @@
 import { FaMapMarkerAlt } from "react-icons/fa";
-import maps from "../images/img-booking/maps.png";
+import maps from "../assets/images/img-booking/maps.png";
 import { useParams } from "react-router-dom";
 import { useState, useContext } from "react";
 
@@ -350,7 +350,7 @@ const HotelDetails = () => {
                   <div className="md:w-[35%]">
                     <div>
                       <div className="pt-4">
-                        <img src={ maps} alt="maps" />
+                        <img src={maps} alt="maps" />
                       </div>
                     </div>
                     {/* reservation form */}
